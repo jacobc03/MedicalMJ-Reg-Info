3ea0c1474dcb5421da8427b893dba6cf1d565275
Jacob <jacob@Jacobs-MacBook-Air.local>
2017-03-21 00:52:52 -0400

    styled state page

--- app/mjStateRegInfo/features/home/home.controller.js
+++ app/mjStateRegInfo/features/home/home.controller.js
@@ -8,6 +8,17 @@ angular
 		//set a variable to store the date from db
 		vm.stateRegInfo= [];
 
+		// vm.state = {
+
+		// };
+
+		// function selectState(stateName) {
+		// 	stateinfo.queryfunction()
+
+		// 	vm.state.name = data.state;
+			
+		// }
+
 		//use the stateInfo service to get all state info from the db
 		stateInfo.getStateInfo(vm.stateRegInfo);
 		console.log(vm.stateRegInfo);
